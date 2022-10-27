1161d048234d8a20ff48054cf92658e30de03867
ethan hobson <ethan.james.hobson@gmail.com>
2022-10-27 14:08:52 -0600

    main functionality done next - bootstrap and styling

--- index.js
+++ index.js
@@ -1,92 +1,15 @@
-// let getcourses = document.querySelector('#getCourses').addEventListener('click', getTees)
-// let id = ''
+const parrow = document.querySelector('.parrow')
+const yardrow = document.querySelector('.yardrow')
+const hcprow = document.querySelector('.hcprow')
+const playerrow = document.querySelector('.playerrow')
+let tabletemplatepar = document.getElementById('tabletemplatepar')
+let players = document.getElementById('players')
+let player = document.getElementById('player')
+let playerform = document.querySelector('.playerform')
 
-// const LOCAL_STORAGE_LIST_KEY = 'list.list'
-// const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'list.SelectedListId'
-// let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
-// let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+let playerCount = 0
+let teeBoxSelectHtml = ''
 
-// function saveandrender() {
-// save()
-// render()
-// }
-
-// function render() {
-//     clearElement(listsContainer)
-//     renderlists()
-//     let selectedList = lists.find(list => list.id === selectedListId)
-//     if (selectedListId === null) {
-//       todolistdisplay.style.visibility = 'hidden'
-//     } else {
-//       todolistdisplay.style.visibility = 'visible'
-//       todotitle.innerText = selectedList.name
-//       clearElement(todolist)
-//       rendertasks(selectedList)
-//     }
-// }
-
-// function save() {
-// localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
-// localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
-// }
-// function getAvailableCourses() {
-//     fetch('https://golf-courses-api.herokuapp.com/courses/',
-//         {
-//             method: 'GET',
-//             headers: { 'Content-Type': 'application/json' },
-//         })
-//         .then(res => {
-//             return res.json()
-//         })
-//         .then(data => {
-//             resolve(data.courses)
-//         })
-// }
-// getAvailableCourses();
-
-// function resolve(courses) {
-//     let courseOptionsHtml = ''
-//     courses.forEach((course) => {
-//         courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`
-//     });
-//     document.getElementById('course-select').innerHTML = courseOptionsHtml
-//     id = document.getElementById('course-select').value
-//     console.log(id)
-// }
-// // document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
-// function getTees() {
-//     fetch('https://golf-courses-api.herokuapp.com/courses/${id}',
-//         {
-//             method: 'GET',
-//             headers: { 'Content-Type': 'application/json' },
-//         })
-//         .then(res => {
-//             return res.json()
-//         })
-//         .then(data => {
-//             resolve(data.data)
-//             console.log(data)
-//         })
-//         console.log('return')
-//         saveandrender()
-// }
-// function resolve(id) {
-//     let teeOptionsHtml = ''
-//     id.forEach((tee) => {
-//         teeOptionsHtml += `<option value="${tee.id}">${tee.name}</option>`
-//     });
-//     document.getElementById('tee-select').innerHTML = teeOptionsHtml
-//     id = document.getElementById('tee-select').value
-//     console.log(id)
-// }
-const parrow = document.querySelector('#parrow')
-const tasktemplatepar = document.querySelector('#tasktemplatepar')
-let currentTee = 0
-  let currentCourse = 18300
-  let playerCount = 0
-  let players = []
-  let teeBoxSelectHtml = ''
-//   const selectCourse = getElementById('#course-select')
   class Player {
       constructor(name, scores = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) {
           this.name = name;
@@ -121,12 +44,7 @@ let currentTee = 0
     
   }
 
-//   function getCourseTeeBoxes(courseId) {
-//     fetch(`https://golf-courses-api.herokuapp.com/courses/${currentCourse}`)
-//         .then(response => response.json())
-//         .then(data => data.data.holes[0].teeBoxes);
-//         console.log(1)
-//   }
+
   function renderTeeBoxes(teeBoxes) {
     let teeBoxSelectHtml = '';
     teeBoxes.forEach(function (teeBoxItem, index) {
@@ -138,14 +56,6 @@ let currentTee = 0
     document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
 }
 
-function newTableData(par, holes, teeBoxes) {
-    const tableelement = document.importNode(tabletemplatepar.content, true)
-    teeBoxes.forEach(element => {
-        document.queryselector('par') = element
-        parrow.appendChild(tableelement)
-    })
-
-}
 
 (async function initializeLoad() {
     const courses = await getAvailableCourses()
@@ -153,14 +63,82 @@ function newTableData(par, holes, teeBoxes) {
     printCourses(courses);
 
     document.getElementById('course-select').addEventListener('change',async (e) => {
-        const courseId = e.target.value
+        var courseId = e.target.value
         const courseInfo = await getCourseInfo(courseId);
         const teeBoxes = courseInfo.holes[0].teeBoxes
         const par = courseInfo.holes[0].teeBoxes[0].par
         const holes = courseInfo.holes[0]
-
+        const element = document.querySelectorAll("td")
+        element.forEach(td => {
+          td.remove()
+        })
         renderTeeBoxes(teeBoxes)
-        newTableData(par, holes, teeBoxes)
-    })     
+        newTableData(courseInfo.holes)
+    })   
 })()
+
+playerform.addEventListener('submit', e => {
+  e.preventDefault()
+  const newplayer = document.getElementById("newplayer").value
+  if (newplayer == null || newplayer === '') return
+  nplayer = newplayer
+  renderplayernames(nplayer)
+  newplayer.value = null
+  
+})
+
+function newTableData(holes) {
+   const pars = getListOfPar(1, holes)
+   const yards = getListOfYards(1, holes)
+   const hcp = getListOfHcp(1, holes)
+   document.querySelector('#tee-box-select').addEventListener('change', async (e) => {
+    const element = document.querySelectorAll("td")
+    element.forEach(td => {
+      td.remove()
+    })
+    renderpar(pars)
+    renderyards(yards)
+    renderhcp(hcp)
+    
+})  
+}
+
+function getListOfPar(teeTypeId, holes) {
+  return holes.map(hole => hole.teeBoxes.find(teeBox => teeBox.teeTypeId === teeTypeId).par)
+}
+function getListOfYards(teeTypeId, holes) {
+  return holes.map(hole => hole.teeBoxes.find(teeBox => teeBox.teeTypeId === teeTypeId).yards)
+}
+function getListOfHcp(teeTypeId, holes) {
+  return holes.map(hole => hole.teeBoxes.find(teeBox => teeBox.teeTypeId === teeTypeId).hcp)
+}
+
+function renderpar(pars){
+  pars.forEach((parItem, index) => {
+    const parElement = tabletemplatepar.content.querySelector("td").cloneNode(true);
+    parElement.textContent = parItem;
+    parrow.appendChild(parElement);
+  })
+}
+function renderyards(yards){
+  yards.forEach((yardItem, index) => {
+    const yardElement = tabletemplatepar.content.querySelector("td").cloneNode(true);
+    yardElement.textContent = yardItem;
+    yardrow.appendChild(yardElement);
+  })
+}
+function renderhcp(hcp){
+  hcp.forEach((hcpItem, index) => {
+    const hcpElement = tabletemplatepar.content.querySelector("td").cloneNode(true);
+    hcpElement.textContent = hcpItem;
+    hcprow.appendChild(hcpElement);
+  })
+}
+function renderplayernames(nplayer){
+  const playerElement = players.content.querySelector('tr').cloneNode(true);
+  playerElement.querySelector("th").textContent = nplayer.toUpperCase()
+  playerrow.appendChild(playerElement);
+}
+
+
 
